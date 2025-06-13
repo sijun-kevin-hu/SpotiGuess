9ecec8aebbc8a800c6cd03a873d5589402a616cd
thekevinhu <22kevinhu@gmail.com>
2025-06-12 23:46:31 -0400

    fix: Update getTopTracks method to use FullTrack type for better type safety

--- frontend/src/app/services/spotify-service.ts
+++ frontend/src/app/services/spotify-service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Track } from '../models/tracks-model';
+import { FullTrack } from '../models/tracks-model';
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
   constructor(private http: HttpClient) { }
 
-  getTopTracks(): Observable<Track[]> {
-    return this.http.get<Track[]>('/api/spotifyapi/top-tracks');
+  getTopTracks(): Observable<FullTrack[]> {
+    return this.http.get<FullTrack[]>('/api/spotifyapi/top-tracks');
   }
 }
